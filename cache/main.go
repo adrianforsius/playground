2f80d2a2a4aca2c0954ffac0321de81d215d492e
Adrian Forsius <adrianforsius@gmail.com>
2023-05-02 10:11:02 +0200

    Add garbage collection and expiration

--- cache/main.go
+++ cache/main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"hash/fnv"
 	"sync"
+	"time"
 )
 
+const EXPIRATION = 1
+const GARBAGE_COLLECTOR = 2
+
+type item struct {
+	expire time.Time
+	value  string
+}
+
 type bucket struct {
-	items map[int]string
+	items map[int]item
 	mu    *sync.RWMutex
 }
 
-func (b bucket) Get(key int) string {
+func (b bucket) Get(key int) (string, error) {
 	b.mu.RLock()
 	defer b.mu.RUnlock()
-	return b.items[key]
+	item, ok := b.items[key]
+	if !ok {
+		return "", errors.New("not found")
+	}
+	return item.value, nil
 }
 
 func (b bucket) Add(key int, val string) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	b.items[key] = string(val)
+	b.items[key] = item{time.Now().Add(time.Second * EXPIRATION), val}
 }
 
 type Cache struct {
@@ -36,7 +50,7 @@ func (c *Cache) Add(val string) int {
 	return key
 }
 
-func (c *Cache) Get(key int) string {
+func (c *Cache) Get(key int) (string, error) {
 	return c.buckets[key%256].Get(key)
 }
 
@@ -45,9 +59,27 @@ func New() *Cache {
 	for i := 0; i < 256; i++ {
 		c.buckets[i] = bucket{
 			mu:    &sync.RWMutex{},
-			items: make(map[int]string),
+			items: make(map[int]item),
 		}
 	}
+
+	go func() {
+		for {
+			time.Sleep(time.Second * GARBAGE_COLLECTOR)
+			c.mu.Lock()
+			for _, bucket := range c.buckets {
+				for idx, itm := range bucket.items {
+					bucket.mu.Lock()
+					if itm.expire.Before(time.Now()) {
+						bucket.items[idx] = item{}
+					}
+					bucket.mu.Unlock()
+				}
+			}
+			c.mu.Unlock()
+		}
+	}()
+
 	return &c
 }
 
@@ -57,7 +89,24 @@ func main() {
 	key2 := c.Add("hearaasjdas jkdasdj k asd  ASssSDASssda ")
 	key3 := c.Add("hearaasjdas jkdasdj k ")
 
-	fmt.Println(c.Get(key1))
-	fmt.Println(c.Get(key2))
-	fmt.Println(c.Get(key3))
+	var err error
+	val1, err := c.Get(key1)
+	if err != nil {
+		fmt.Println("error with key", key1)
+	}
+	fmt.Println("val1", val1)
+
+	time.Sleep(time.Second * 3)
+
+	val2, err := c.Get(key2)
+	if err != nil {
+		fmt.Println("error with key", key2)
+	}
+	fmt.Println("val2", val2)
+
+	val3, err := c.Get(key3)
+	if err != nil {
+		fmt.Println("error with key", key3)
+	}
+	fmt.Println("val3", val3)
 }

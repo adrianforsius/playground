2064b96163500d634ba009fddf82509ff22730ad
Adrian Forsius <adrianforsius@gmail.com>
2023-05-03 09:54:44 +0200

    Add weight to cache

--- cache/main.go
+++ cache/main.go
@@ -38,12 +38,14 @@ func (b bucket) Add(key int, val string) {
 }
 
 type Cache struct {
+	// add buckets to reduce lock contention
 	buckets map[int]bucket
 	mu      sync.RWMutex
 }
 
 func (c *Cache) Add(val string) int {
 	h := fnv.New32a()
+	time.Sleep(time.Millisecond * 100)
 	h.Write([]byte(val))
 	key := int(h.Sum32())
 	c.buckets[key%256].Add(key, val)
@@ -51,6 +53,7 @@ func (c *Cache) Add(val string) int {
 }
 
 func (c *Cache) Get(key int) (string, error) {
+	time.Sleep(time.Millisecond * 100)
 	return c.buckets[key%256].Get(key)
 }
 
@@ -83,11 +86,100 @@ func New() *Cache {
 	return &c
 }
 
+type add struct {
+	key   int
+	value string
+}
+
+type get struct {
+	key   int
+	value string
+	err   error
+}
+
+// Weight is to balance read and write on a 80/20 basis
+func NewWeight(cache Cacher) *Weight {
+	addChan := make(chan add)
+	getChan := make(chan get)
+	go func() {
+		read := 0
+		for {
+			if read < 4 {
+				select {
+				case r := <-getChan:
+					val, err := cache.Get(r.key)
+					getChan <- get{value: val, err: err}
+					read++
+				default:
+				}
+			}
+
+			read = 0
+			select {
+			case r := <-addChan:
+				addChan <- add{key: cache.Add(r.value)}
+			default:
+			}
+
+			select {
+			case r := <-addChan:
+				addChan <- add{key: cache.Add(r.value)}
+			case r := <-getChan:
+				val, err := cache.Get(r.key)
+				getChan <- get{value: val, err: err}
+			default:
+			}
+
+		}
+
+	}()
+
+	return &Weight{cache: cache, get: getChan, add: addChan}
+}
+
+type Weight struct {
+	cache Cacher
+	add   chan add
+	get   chan get
+}
+
+func (w *Weight) Add(val string) int {
+	w.add <- add{value: val}
+	resp := <-w.add
+	return resp.key
+}
+
+func (w *Weight) Get(key int) (string, error) {
+	w.get <- get{key: key}
+	resp := <-w.get
+	return resp.value, resp.err
+}
+
+type Cacher interface {
+	Add(string) int
+	Get(int) (string, error)
+}
+
 func main() {
-	c := New()
+	c := NewWeight(New())
+	fmt.Println("adding values")
 	key1 := c.Add("hearaasjdas jkdasdj k asd ")
 	key2 := c.Add("hearaasjdas jkdasdj k asd  ASssSDASssda ")
 	key3 := c.Add("hearaasjdas jkdasdj k ")
+
+	for i := 0; i < 5_000; i++ {
+		go func(i int) {
+			val, err := c.Get(key1)
+			if err != nil {
+				fmt.Println("error with key", key1, val, err)
+			}
+			fmt.Println("got value!")
+			if i%3 == 0 {
+				c.Add(fmt.Sprintf("hearaasjdas jkdasdj k  %d", i))
+			}
+			fmt.Println("added value!")
+		}(i)
+	}
 
 	var err error
 	val1, err := c.Get(key1)
